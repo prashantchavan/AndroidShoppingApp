f8d3b1b9f616074794d63b99808189651a73f8f7
prashantchavan <prashant.j.chavan@gmail.com>
2017-12-08 21:23:38 +0530

    First Commit

---
+++ app/src/main/java/com/strider/admin/shoppingapp/App.java
@@ -0,0 +1,47 @@
+package com.strider.admin.shoppingapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+/**
+ * Created by Admin on 12/8/2017.
+ */
+
+public class App {
+
+    public static final String LOGIN = "login";
+
+    public static void showToast(Context context ,String message)
+    {
+        Toast.makeText(context , message , Toast.LENGTH_LONG).show();
+    }
+
+    public static void setString(Context context , String key , String value)
+    {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putString(key,value);
+        editor.commit();
+    }
+
+    public static String getString(Context context , String key , String defaultValue)
+    {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getString(key,defaultValue);
+    }
+
+    protected static void setLogin(Context context , String key , Boolean value)
+    {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean(key,value);
+        editor.commit();
+    }
+
+    protected  static void isLoggedIn(Context context , String key)
+    {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        sharedPreferences.getBoolean(key , false);
+    }
+
+}
